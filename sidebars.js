29878afbd7047bca39edd9d8f07b327f533bd0bd
Liudmila Nemkova <lnemkova@adapty.io>
2024-07-03 14:07:19 +0200

    First upload

---
+++ sidebars.js
@@ -0,0 +1,31 @@
+// sidebars.js
+
+/** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
+const sidebars = {
+  tutorialSidebar: [
+    {
+      type: 'doc',
+      id: 'what-is-adapty',
+      label: "What is Adapty?",
+    },
+    {
+      type: 'category',
+      label: 'Migrate to Adapty',
+      link: {
+        type: 'doc',
+        id: 'migrate-to-adapty-from-another-solutions',
+      },
+      collapsible: true,
+      collapsed: false,
+      items: [
+        {
+          type: 'doc',
+          id: 'migration-from-revenuecat',
+          label: 'Migration from RevenueCat',
+        },
+      ],
+    },
+  ],
+};
+
+module.exports = sidebars;
